c5953759baa6cdaa45654b891eef41e03a9556cf
Rafael Cardoso <rafa_cardoso2001@hotmail.com>
2021-05-13 14:59:28 -0300

    Tweaks
    -Improved site style by adding a middle column and background.
    -Some others minor tweaks.
    -Added an animated loader.
    -Improved fetch functions(not fully completed).

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,18 +1,57 @@
 function run() {
+    loader.style.display = 'block';
     var apibase = "https://pokeapi.co/api/v2/pokemon/";
-    var input = document.getElementById("inputpoke").value;
+    var input = "error"
+    if (document.getElementById("inputpoke").value != ""){
+      var input = document.getElementById("inputpoke").value;
+    }
+
     var fetchpokemon = apibase + input;
     runfetch(fetchpokemon);
 
 }
+async function response(fetchpokemon){
 
-function runfetch(fetchpokemon) {
-    fetch(fetchpokemon)
+}
+async function runfetch(fetchpokemon) {
+  let response = await fetch(fetchpokemon);
+
+  if (response.ok){
+    loader.style.display = 'none';
+    let json = await response.json();
+    document.getElementById("pokemonsprite").src = await json.sprites.front_default;
+  }else{
+    loader.style.display = 'none';
+    alert("HTTP-Error: " + response.status);
+  }
+
+
+
+    /*fetch(fetchpokemon)
         .then(res => res.json())
         .then(result => {
-            console.log(result);
-            document.getElementById("demo").innerHTML = result.stats[0].base_stat;
+            console.log(result.status);
+            document.getElementById("demo").innerHTML = result.name;
             document.getElementById("pokemonsprite").src = result.sprites.front_default;
-        })
-    sprites.front_default
+            showstats();
+        })*/
 }
+
+/*function showstats(){
+  var spr = document.getElementById("pokemonsprite");
+  if (spr.src != null){
+    spr.style.display = "block";
+  }
+  else{
+    spr.style.display = "none";
+  }
+}
+
+var input = document.getElementById("inputpoke");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+
+   event.preventDefault();
+   run();
+  }
+});*/
